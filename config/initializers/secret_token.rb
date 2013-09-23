d0237131bf33d27b3094f00adfce1e7ff11f0a9d
Max Liu <dr.max.liu@gmail.com>
2013-09-23 02:44:14 -0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Book3::Application.config.secret_token = 'c11531819857987893a9753421d0783b67d773daba51ff34acf52027a172758dc9ac4a54f2d8404b443ea68de74ac215e9e07cac2505e1f397f9896c058feedd'
